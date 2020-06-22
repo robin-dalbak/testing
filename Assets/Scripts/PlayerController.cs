a7af0ecbb913dcdd3b914af32bf10646acd5f279
AJ <angela.jakobsen@gmail.com>
2020-06-22 15:05:01 +0200

    Added player controller script
    Connected script to player object

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    public float movement = 0f;
+    private Rigidbody2D rigidBody;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+
+        if(movement >0f) {
+
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+
+        }
+
+    }
+}
