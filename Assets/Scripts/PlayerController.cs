7c59f7af87b5c3b159d80caab8eabc65166be3a4
AJ <angela.jakobsen@gmail.com>
2020-06-24 11:14:17 +0200

    Updated player
    Moving player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed = 8f;
     public float movement = 0f;
     private Rigidbody2D rigidBody;
-
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
 
 
 
@@ -22,18 +25,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
         movement = Input.GetAxis("Horizontal");
 
         if(movement >0f) {
-
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
         }
 
-        else if (movement < 0f)
+        else if (movement < 0f) {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+
+        else {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+
+        if(Input.GetButtonDown ("Jump") && isTouchingGround)
         {
-            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
-
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
         }
-
     }
 }
